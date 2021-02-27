b0ac7034d240b84403fa171f3a69252eab6452c0
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-02-27 10:52:26 -0500

    Updated html, css and js.

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-$(document).ready(function () {
-  //moment date and clock at top of page
-  let today = moment();
-  $("#currentDay").text(today.format("dddd, MMMM Do"));
+//moment.js for date at top of page
+let today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do"));
 
-  $(".saveBtn").on("click", function() {
+$(document).ready(function () {
+
+
+  //save buttons event listener
+  $(".saveBtn").on("click", function () {
     let value = $(this).siblings(".description").val();
     console.log(value);
 
     let time = $(this).siblings(".hour").attr("id");
     console.log(time);
-    localStorage.setItem(time, value)
-  })
+    localStorage.setItem(time, value) //local storage for each time block - sets/saves event input text
+  });
 
+  //local storage for each time block - takes saved event input text and gets them to be stored
   $(".nineAm").val(localStorage.getItem("9am"));
   $(".tenAm").val(localStorage.getItem("10am"));
+  $(".elevenAm").val(localStorage.getItem("11am"));
+  $(".twelvePm").val(localStorage.getItem("12am"));
+  $(".onePm").val(localStorage.getItem("1pm"));
+  $(".twoPm").val(localStorage.getItem("2pm"));
+  $(".threePm").val(localStorage.getItem("3pm"));
+  $(".fourPm").val(localStorage.getItem("4pm"));
+  $(".fivePm").val(localStorage.getItem("5pm"));
 
+  // moment.js for getting hourly time
+  let currentTime = moment().format("h");
+  console.log(currentTime);
 
-//   moment time for time blocks
-  let time = moment().format("h");
-  console.log(time);
-  function timeBlocks() {
-//.each(.time-block) {
-    //grab only numeric value from each time block (9am, 10am, etc)
-    //remove "am" and convert into a number
-    //let parseInt("string")
-
-    // if else statement
-    // if (each) block time < addClass(past) use (this)
-    // if = present
-    // else = future
-// }
-  }
-});
+  let timeBlock = $(".time-block");
+    timeBlock.each(function() {
+      
+      
+      //attaching CSS to the time for past, present and future
+      if (currentTime === timeBlock) {
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        
+      } else if (currentTime < timeBlock) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        
+      } else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+      }});
+      
+    });
+    
+    
+    
+    // let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+    //   console.log( timeBlock, currentTime);
+    // .each(.time-block) {
+    //   grab only numeric value from each time block (9am, 10am, etc)
+    //   remove "am" and convert into a number
+    //   let parseInt("string")
+    //   if (each) block time < addClass(past) use (this)
